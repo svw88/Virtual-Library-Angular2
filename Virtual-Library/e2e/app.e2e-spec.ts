af0f5cdf1fb7ffc2c94cb968a6326ab0ba046d0b
Stephanus van Wyk <svw88@hotmail.com>
2017-06-08 14:10:12 -0300

    tests commit

--- Virtual-Library/e2e/app.e2e-spec.ts
+++ Virtual-Library/e2e/app.e2e-spec.ts
@@ -8,7 +8,41 @@ describe('virtual-library App', () => {
   });
 
   it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+    page.navigateTo("/");
+    expect(page.getParagraphText()).toEqual('Welcome To The Virtual Library');
   });
+
+   it('should retrieve books', () => {
+    page.navigateTo("/search/a");
+    expect(page.getCount('bookListObject')).toEqual(5);
+  });
+
+   it('should retrieve single book', () => {
+    page.navigateTo("/search/a");
+    var h1 =  page.getFirstElement('h4').getAttribute('innerText');
+    page.getFirstElement('bookListObject').click();
+    expect(page.getFirstElement('h4').getAttribute('innerText')).toEqual(h1);
+
+  });
+  
+  it('should retrieve favorites', () => {
+    page.navigateTo("/favorites");
+    expect(page.getCount('bookListObject')).toEqual(5);
+  });
+
+   it('should add favorites', () => {
+    page.navigateTo("/search/a"); 
+    page.getFirstElement('i').click();
+    page.navigateTo("/favorites");   
+    expect(page.getCount('bookListObject')).toEqual(6);
+  });
+
+   it('should remove favorites', () => {
+    page.navigateTo("/search/a"); 
+    page.getFirstElement('i').click();
+    page.navigateTo("/favorites");   
+    expect(page.getCount('bookListObject')).toEqual(5);
+  });
+  
+
 });
--- Virtual-Library/e2e/app.po.ts
+++ Virtual-Library/e2e/app.po.ts
@@ -1,11 +1,19 @@
 import { browser, by, element } from 'protractor';
 
 export class VirtualLibraryPage {
-  navigateTo() {
-    return browser.get('/');
+  navigateTo(url) {
+    return browser.get(url);
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('h1')).getText();
+  }
+  
+  getCount(elementType) {
+  return element.all(by.css(elementType)).count();
+  }
+
+  getFirstElement(elementType) {
+   return element(by.css(elementType));
   }
 }
